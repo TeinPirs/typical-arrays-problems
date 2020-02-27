9115fd6bb1bf50c24cdadfb60c62a775815108c8
Tein Pirs <tein.pirs@mail.ru>
2020-02-27 18:16:21 +0300

    feat: index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (!array || array.length == 0) {
+        return 0
+    } else
+        return array.reduce((min, current) => min < current ? min : current, 0)
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (!array || array.length == 0) {
+        return 0
+    } else
+        return array.reduce((max, current) => max > current ? max : current, 0)
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (!array || array.length == 0) {
+        return 0
+    } else
+        return array.reduce((sum, current) => sum + current, 0) / array.length;
 }
