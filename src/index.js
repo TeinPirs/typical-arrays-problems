fc99ef499030352a8b492ffb2bb555eddfd60456
Kalinichenko Anatoliy <tein.pirs@mail.ru>
2021-03-10 00:50:19 +0300

    fix: I wrote it clearly for myself

--- src/index.js
+++ src/index.js
@@ -1,20 +1,39 @@
 exports.min = function min(array) {
     if (!array || array.length == 0) {
-        return 0
-    } else
-        return array.reduce((min, current) => min < current ? min : current, 0)
-}
+        return 0;
+    } else {
+        let p = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] < p) {
+                p = array[i];
+            }
+        }
+        return p;
+    }
+};
 
 exports.max = function max(array) {
     if (!array || array.length == 0) {
-        return 0
-    } else
-        return array.reduce((max, current) => max > current ? max : current, 0)
-}
+        return 0;
+    } else {
+        let p = array[0];
+        for (let i = 1; i < array.length; i++) {
+            if (array[i] > p) {
+                p = array[i];
+            }
+        }
+        return p;
+    }
+};
 
 exports.avg = function avg(array) {
     if (!array || array.length == 0) {
-        return 0
-    } else
-        return array.reduce((sum, current) => sum + current, 0) / array.length;
-}
+        return 0;
+    } else {
+        let p = 0;
+        for (let i = 0; i < array.length; i++) {
+            p += array[i];
+        }
+        return p / array.length;
+    }
+};
